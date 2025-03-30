5e3e2068f1b967a986b12b3e49c95b11b5e6b5c5
ezerssss <magbanuaezra@gmail.com>
2025-03-30 22:16:30 +0800

    [Config][Ezra] hello world

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Home() {
     return (
         <View>
-            <Text className="font-bold text-xl">Home</Text>
+            <Text className="font-bold text-xl">Hello World</Text>
         </View>
     );
 }
