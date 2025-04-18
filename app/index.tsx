677bc8591000615966e58a1463998fc03a9f370a
ezerssss <magbanuaezra@gmail.com>
2025-04-18 22:28:03 +0800

    fukcing hell

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { testOnCall } from "@/firebase/functions";
 import useLogin from "@/hooks/useLogin";
 import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
-import { useRouter } from "expo-router";
+import { Href, useRouter } from "expo-router";
 
 export default function Home() {
     const { isLoading, handleLogin } = useLogin();
@@ -31,7 +31,10 @@ export default function Home() {
             >
                 <Text>Test Cloud Functions</Text>
             </TouchableOpacity>
-            <TouchableOpacity className="self-baseline rounded border px-4 py-2" onPress={() => router.push('/seller-form/seller-form-page')}>
+            <TouchableOpacity
+                className="self-baseline rounded border px-4 py-2"
+                onPress={() => router.push("/seller/seller-form")}
+            >
                 <Text>Go to Seller Form</Text>
             </TouchableOpacity>
         </View>
