10f391005a1707e30b02202c9ad33cca5e1daaf8
ezerssss <magbanuaezra@gmail.com>
2025-04-20 14:32:55 +0800

    [Adjustment][Ezra] change user schema following backend source of truth

--- types/user.ts
+++ types/user.ts
@@ -1,9 +1,17 @@
+import { CampusEnum } from "@/enums/campus";
 import { z } from "zod";
 
 export const UserDataSchema = z.object({
-    uid: z.string().min(1),
-    name: z.string().min(1),
+    id: z.string().min(1),
     email: z.string().email(),
+    name: z
+        .string()
+        .trim()
+        .min(1, { message: "Name is required" })
+        .max(100, { message: "Name can only be 100 letters long." }),
+    imageUrl: z.string().url().nullable(),
+    campus: CampusEnum,
     dateCreated: z.string().datetime(),
+    dateUpdated: z.string().datetime(),
 });
-export const UserDataType = UserDataSchema._type;
+export type UserDataType = z.infer<typeof UserDataSchema>;
