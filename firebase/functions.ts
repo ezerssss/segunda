f6c207cf958fdbd11e2084279e856fc46b939d1c
Andry T <andryjosephtumacole@gmail.com>
2025-04-23 00:59:59 +0800

    [Fix][AJ] Readded the accidentally deleted piece of code (setup user) in functions.ts

--- firebase/functions.ts
+++ firebase/functions.ts
@@ -1,4 +1,10 @@
-import { getFunctions } from "@react-native-firebase/functions";
+import { getFunctions, httpsCallable } from "@react-native-firebase/functions";
 import app from ".";
+import { SetUpUserRequestType } from "@/types/user";
+import { GenericResponseMessage } from "@/types/response";
 
 const functions = getFunctions(app, "asia-southeast1");
+export const setUpUser = httpsCallable<
+    SetUpUserRequestType,
+    GenericResponseMessage
+>(functions, "setUpUser");
