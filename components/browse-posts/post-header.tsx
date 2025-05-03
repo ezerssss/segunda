3b38abcbd2511f62f86a9280cb70c7a3faac28e3
rianluisx <rcmabait@up.edu.ph>
2025-05-03 13:41:41 +0800

    [Rian][Frontend] browse-post: changed type actionsheet to any, added key for pressable image

--- components/browse-posts/post-header.tsx
+++ components/browse-posts/post-header.tsx
@@ -12,9 +12,7 @@ interface PostHeaderProps {
 
 function PostHeader(props: PostHeaderProps) {
     const { postId, userName, userImageUrl } = props;
-    const actionSheetRef = useRef<any>(null); // never mind lang ito type script error (mag work man code di ko alam pano i debug HAHAH)
-    // bandaid fix ko was typeOf ActionSheet pero
-    // yung mangyayari kay yung mga methods ng actionsheetRef kay mag typescript error din
+    const actionSheetRef = useRef<any>(null);
 
     function handleReport() {
         console.log("REPORTED KA NA: ", userName);
