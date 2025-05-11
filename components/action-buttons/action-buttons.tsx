42dc0bb2b02ed2b0b7c85d4e15b12ec9b22c041f
Andry T <andryjosephtumacole@gmail.com>
2025-05-12 03:23:50 +0800

    [Frontend][Integration] Integrated action buttons to browse post

--- components/action-buttons/action-buttons.tsx
+++ components/action-buttons/action-buttons.tsx
@@ -1,38 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import SellerActionButton from "./seller-action-button";
 import StealerActionButtons from "./stealer-action-buttons";
 import MinerActionButton from "./miner-action-button";
-import { View, Text } from "react-native";
 import { ItemType } from "@/types/item";
+import { UserContext } from "@/contexts/userContext";
 
 interface ActionButtonsProps {
     item: ItemType;
 }
 
 function ActionButtons(props: Readonly<ActionButtonsProps>) {
+    const { user } = useContext(UserContext);
     const { item } = props;
+    const isSeller = user?.id === item.sellerId;
 
-    return (
-        <View className="flex-1">
-            <View className="w-full flex-row justify-between">
-                <StealerActionButtons item={item}></StealerActionButtons>
-            </View>
-            <View className="my-4"></View>
-            <View className="w-full flex-row justify-between">
-                <MinerActionButton item={item}></MinerActionButton>
-            </View>
-            <View className="mt-10">
-                <SellerActionButton item={item}></SellerActionButton>
-            </View>
+    function getActionButtons() {
+        if (isSeller) return <SellerActionButton item={item} />;
+        if (item.miner === null) return <MinerActionButton item={item} />;
+        return <StealerActionButtons item={item} />;
+    }
 
-            <View className="mt-5 h-12 flex-row justify-between bg-blue-50">
-                <View className="w-1/2 bg-red-50">
-                    <Text>Random area</Text>
-                </View>
-                <StealerActionButtons item={item}></StealerActionButtons>
-            </View>
-        </View>
-    );
+    return getActionButtons();
 }
 export default ActionButtons;
