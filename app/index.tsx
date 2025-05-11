2f5867b10c360fa8b6a7a96928ff60ce280a0e66
Andry T <andryjosephtumacole@gmail.com>
2025-05-12 03:25:46 +0800

    [Cleanup][AJ] Removed test page for action buttons

--- app/index.tsx
+++ app/index.tsx
@@ -27,12 +27,6 @@ export default function Home() {
             </TouchableOpacity>
             <TouchableOpacity
                 className="self-baseline rounded border px-4 py-2"
-                onPress={() => router.push("/test-action-buttons")}
-            >
-                <Text>Test Action Buttons</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                className="self-baseline rounded border px-4 py-2"
                 onPress={() => router.push("/browse-posts")}
             >
                 <Text>Browse Posts</Text>
