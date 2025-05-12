fe1b3c821cf4c8d8bfc2f39ff4b767749479f726
jrolana <olanajhoanna@gmail.com>
2025-05-12 17:26:01 +0800

    [WIP][Jhoanna] material top bar navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,99 @@
 import { Text } from "react-native";
-import { Redirect, Tabs } from "expo-router";
+import { Redirect, withLayoutContext } from "expo-router";
 import { useContext } from "react";
 import { UserContext } from "@/contexts/userContext";
+import { Icon, Avatar } from "@ui-kitten/components";
+
+import {
+    MaterialTopTabNavigationEventMap,
+    MaterialTopTabNavigationOptions,
+    createMaterialTopTabNavigator,
+} from "@react-navigation/material-top-tabs";
+import { ParamListBase, TabNavigationState } from "@react-navigation/native";
+
+const { Navigator } = createMaterialTopTabNavigator();
+
+export const TopTabs = withLayoutContext<
+    MaterialTopTabNavigationOptions,
+    typeof Navigator,
+    TabNavigationState<ParamListBase>,
+    MaterialTopTabNavigationEventMap
+>(Navigator);
 
 export default function AppLayout() {
     const { user, isUserLoading } = useContext(UserContext);
 
-    // You can keep the splash screen open, or render a loading screen like we do here.
     if (isUserLoading) {
         return <Text>Loading...</Text>;
     }
 
-    // Only require authentication within the (app) group's layout as users
-    // need to be able to access the (auth) group and sign in again.
     if (!user) {
-        // On web, static rendering will stop here as the user is not authenticated
-        // in the headless Node process that the pages are rendered in.
         return <Redirect href="/login" />;
     }
 
-    // This layout can be deferred because it's not the root layout.
     return (
-        <Tabs screenOptions={{ headerShown: false }}>
-            <Tabs.Screen name="home/index" />
-        </Tabs>
+        <TopTabs>
+            {/* <TopTabs.Screen
+                name="home/index"
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: () => <Icon name="home-outline" />,
+                }}
+            /> */}
+            <TopTabs.Screen
+                name="my-items"
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: () => <Icon name="archive-outline" />,
+                }}
+            />
+            <TopTabs.Screen
+                name="catalogue"
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: () => <Icon name="book-open-outline" />,
+                }}
+            />
+            <TopTabs.Screen
+                name="notifications"
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: () => <Icon name="bell-outline" />,
+                }}
+            />
+            <TopTabs.Screen
+                name="profile"
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: () => (
+                        <Avatar
+                            source={{
+                                uri: "https://a.thumbs.redditmedia.com/HaXDOt6VCSDYHNnBG2kmo7xArWVWkl8_QgppfGAQP-0.png",
+                            }}
+                            size="tiny"
+                        />
+                    ),
+                }}
+            />
+
+            {/* <TopTabs.Screen
+                name="home/view-post"
+                options={{
+                    href: null,
+                }}
+            />
+            <TopTabs.Screen
+                name="home/seller/form"
+                options={{
+                    href: null,
+                }}
+            />
+            <TopTabs.Screen
+                name="user-setup/index"
+                options={{
+                    href: null,
+                }}
+            /> */}
+        </TopTabs>
     );
 }
