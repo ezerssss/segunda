cf6b8e110626fd46658e6821ccbc288391076865
jrolana <olanajhoanna@gmail.com>
2025-05-17 00:41:54 +0800

    [Enhancement][Olana] Better handling of loading UI

--- app/(protected)/chat/chat-list.tsx
+++ app/(protected)/chat/chat-list.tsx
@@ -1,31 +1,29 @@
 import { FlatList } from "react-native";
 import { Text } from "@ui-kitten/components";
 import ChatThumbnail from "@/components/chat/thumbnail";
-import SkeletonChatThumbnail from "@/components/skeletons/chat-thumbnail";
+import SkeletonChatList from "@/components/skeletons/chat-list";
 
 function ChatList() {
     const isLoading = true;
     const chatsThumbnailData = new Array(15).fill(0);
 
+    if (isLoading) {
+        return <SkeletonChatList />;
+    }
+
     return (
         <FlatList
             data={chatsThumbnailData}
             showsVerticalScrollIndicator={false}
-            renderItem={(item) =>
-                isLoading ? (
-                    <SkeletonChatThumbnail
-                        key={"skeleton-chat-thumbnail" + item.index}
-                    />
-                ) : (
-                    <ChatThumbnail
-                        key={"chat-thumbnail" + item.index}
-                        senderName="Jhoanna Olana"
-                        lastMessageName="Andrea Jones"
-                        lastMessage="I wanna be a tutubi na walang tinatagong bato sa loob ng bato"
-                        isSeller={item.index % 3 === 0}
-                    />
-                )
-            }
+            renderItem={(item) => (
+                <ChatThumbnail
+                    key={"chat-thumbnail" + item.index}
+                    senderName="Jhoanna Olana"
+                    lastMessageName="Andrea Jones"
+                    lastMessage="I wanna be a tutubi na walang tinatagong bato sa loob ng bato"
+                    isSeller={item.index % 3 === 0}
+                />
+            )}
             contentContainerClassName="bg-white m-4"
             ListHeaderComponent={
                 <Text category="h4" className="mb-4">
---
+++ components/skeletons/chat-list.tsx
@@ -0,0 +1,31 @@
+import { View, FlatList } from "react-native";
+import { Text } from "@ui-kitten/components";
+
+export default function SkeletonChatList() {
+    const placeholderData = new Array(12).fill(0);
+
+    return (
+        <FlatList
+            data={placeholderData}
+            showsVerticalScrollIndicator={false}
+            renderItem={(item) => (
+                <View
+                    className="mb-4 flex w-full flex-row items-center"
+                    key={"skeleton-chat-thumbnail" + item.index}
+                >
+                    <View className="h-16 w-16 rounded-full bg-gray-200" />
+                    <View className="ml-4 gap-2">
+                        <View className="h-3 w-40 rounded-full bg-gray-200" />
+                        <View className="h-3 w-80 rounded-full bg-gray-200" />
+                    </View>
+                </View>
+            )}
+            contentContainerClassName="bg-white m-4"
+            ListHeaderComponent={
+                <Text category="h4" className="mb-4">
+                    Chats
+                </Text>
+            }
+        />
+    );
+}
