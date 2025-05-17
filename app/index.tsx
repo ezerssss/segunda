dd23cedada1fe503ebc02e436ea5d17e12d011ad
jrolana <olanajhoanna@gmail.com>
2025-05-17 14:04:37 +0800

    [Fix][Ezra] Has more posts condition

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import { Redirect } from "expo-router";
 
 export default function Root() {
-    // return <Redirect href="/(protected)/chat/chat-list" />;
     return <Redirect href="/(protected)/(tabs)/home" />;
 }
